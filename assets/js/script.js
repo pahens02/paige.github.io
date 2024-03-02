67d9e6b073a77ef4fe8de8d1c1b444e05b270ca6
Paige Hensley <paige@papermountain.org>
2024-03-02 17:45:13 -0500

    More portfolio and testimonial updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,8 +22,7 @@ const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
 
-// modal variable
-const modalImg = document.querySelector("[data-modal-img]");
+// modal variables (without the avatar image)
 const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
 
@@ -33,25 +32,21 @@ const testimonialsModalFunc = function () {
   overlay.classList.toggle("active");
 }
 
-// add click event to all modal items
+// add click event to all testimonials items
 for (let i = 0; i < testimonialsItem.length; i++) {
-
   testimonialsItem[i].addEventListener("click", function () {
-
-    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
     modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
     modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
 
+    // Toggle modal visibility
     testimonialsModalFunc();
-
   });
-
 }
 
-// add click event to modal close button
+// add click event to modal close button and overlay to close the modal
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
+
 
 
 
